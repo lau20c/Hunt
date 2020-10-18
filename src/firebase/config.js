7bb0dea25a3934d03ab775b9bbc86a59718d0944
lau20c <46605726+lau20c@users.noreply.github.com>
2020-10-18 18:36:56 -0400

    Firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB1RTAOAhz2Dcq5YvUMbPiKVWRjsG4bIJA",
+    authDomain: "hunt-ba1d3.firebaseapp.com",
+    databaseURL: "https://hunt-ba1d3.firebaseio.com",
+    projectId: "hunt-ba1d3",
+    storageBucket: "hunt-ba1d3.appspot.com",
+    messagingSenderId: "886391052827",
+    appId: "1:886391052827:web:45c609cb78d67d9f03b6be"
+  };
+
+  export default firebaseConfig;
